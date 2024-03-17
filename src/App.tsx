abf74c245184fe77d8769cca6cd28d4c439739bc
ddozza54 <bananamayong@gmail.com>
2024-03-17 17:49:23 +0900

    styled components 분리 및 Atom import 문 정리

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-import styled from 'styled-components'
-import { motion } from "../node_modules/framer-motion"
 import { useEffect, useState } from 'react';
 import { FaPause, FaPlay } from "react-icons/fa6";
 import ddozza from '../src/assets/Ddo.png';
 import { useRecoilState } from 'recoil';
-import { GOAL_TOTAL, POMO_TOTAL, ROUND_TOTAL, goalAtom, pomoTimeAtom, roundAtom } from './atoms';
+import * as S from "./styles";
+import * as A from './atoms';
 
 const getMinutes = (fullSeconds: number) => {
   return String(Math.floor(fullSeconds / 60)).padStart(2, "0");
@@ -13,11 +12,11 @@ const getSeconds = (fullSeconds: any) => {
   return String(fullSeconds % 60).padStart(2, "0");
 }
 function App() {
-  const [pomoTime, setPomoTime] = useRecoilState(pomoTimeAtom);
+  const [pomoTime, setPomoTime] = useRecoilState(A.pomoTimeAtom);
   const [minutes, setMinutes] = useState(getMinutes(pomoTime))
   const [seconds, setSeconds] = useState(getSeconds(pomoTime))
-  const [round, setRound] = useRecoilState(roundAtom);
-  const [goal, setGoal] = useRecoilState(goalAtom)
+  const [round, setRound] = useRecoilState(A.roundAtom);
+  const [goal, setGoal] = useRecoilState(A.goalAtom)
   const [isTimerWorking, setIsTimerWorking] = useState(false);
   const [IsDisableToSetTime, setIsDisableToSetTime] = useState(false);
   const onPlayBtnClick = () => {
@@ -35,7 +34,7 @@ function App() {
       if (pomoTime === 0) {
         setRound(prev => prev + 1);
         setIsTimerWorking(false);
-        setPomoTime(POMO_TOTAL);
+        setPomoTime(A.POMO_TOTAL);
         setIsDisableToSetTime(false)
       }
       return () => clearInterval(timer);
@@ -43,48 +42,48 @@ function App() {
   }, [pomoTime, isTimerWorking])
 
   useEffect(() => {
-    if (round === ROUND_TOTAL) {
+    if (round === A.ROUND_TOTAL) {
       setGoal(prev => prev + 1)
       setRound(0)
     }
   }, [round])
 
   return (
-    <AppBox>
+    <S.AppBox>
       <img src={ddozza} width='80px' />
       <h2>DDOZZA's Pomodoro</h2>
       <span>Set Pomodoro Time</span>
-      <Timer onSubmit={(event) => {
+      <S.Timer onSubmit={(event) => {
         event.preventDefault();
       }}>
-        <TimeBox
+        <S.TimeBox
           initial={{ opacity: 0, scale: 0.5 }}
           animate={{ opacity: 1, scale: 1 }}
           transition={{ duration: 0.5 }}>
-          <TimeInput onChange={(event) => {
+          <S.TimeInput onChange={(event) => {
             Number(event.target.value) >= 0 ? setMinutes(event.target.value.padStart(2, "0")) : setMinutes("00")
           }} type='number' value={isTimerWorking ? getMinutes(pomoTime) : minutes} disabled={IsDisableToSetTime} />
-        </TimeBox>
+        </S.TimeBox>
         <span>:</span>
-        <TimeBox
+        <S.TimeBox
           key={pomoTime}
           initial={{ opacity: 0, scale: 0.5 }}
           animate={{ opacity: 1, scale: 1 }}
           transition={{ duration: 0.5 }}
         >
-          <TimeInput onChange={(event) => {
+          <S.TimeInput onChange={(event) => {
             Number(event.target.value) >= 0 ? setSeconds(event.target.value.padStart(2, "0")) : setSeconds("00")
           }}
             type='number' value={isTimerWorking ? getSeconds(pomoTime) : seconds} disabled={IsDisableToSetTime} />
-        </TimeBox>
-      </Timer>
-      <PlayBtn onClick={onPlayBtnClick} whileHover={{ scale: 1.2 }}>
+        </S.TimeBox>
+      </S.Timer>
+      <S.PlayBtn onClick={onPlayBtnClick} whileHover={{ scale: 1.2 }}>
         {
           isTimerWorking ? <FaPause />
             : <FaPlay />
         }
-      </PlayBtn>
-      <CounterBox>
+      </S.PlayBtn>
+      <S.CounterBox>
         <TextBox>
           <CountNumber>{round}/{ROUND_TOTAL}</CountNumber>
           <CountTitle>Round</CountTitle>
@@ -93,73 +92,9 @@ function App() {
           <CountNumber>{goal}/{GOAL_TOTAL}</CountNumber>
           <CountTitle>GOAL</CountTitle>
         </TextBox>
-      </CounterBox>
-    </AppBox>
+      </S.CounterBox>
+    </S.AppBox>
   )
 }
-const AppBox = styled.main`
-    width: 30rem;
-    margin-top: 30px;
-    padding: 1rem;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-`;
-const TimeInput = styled.input`
-  border: none;
-  background-color: transparent;
-  width: 100%;
-  margin-left: 6px;
-  font-size: 6rem;
-`
-const Timer = styled.form`
-    display: flex;
-    align-items: center;
-    font-size: 6rem;
-    padding-bottom: 2rem;
-`;
-const TimeBox = styled(motion.div)`
-  width: 8rem;
-  height: 12rem;
-  background-color: #b4c794;
-  padding: 0.5rem;
-  margin: 1rem;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  border-radius: 20px;
-  text-indent: 30%;
-  `;
-const PlayBtn = styled(motion.button)`
-    width: 4rem;
-    height: 4rem;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    border-radius: 50%;
-    font-size: 2rem;
-    border: none;
-    background-color:#dfd6cf;
-    &:hover{
-      cursor: pointer;
-    }
-  `;
-const CounterBox = styled.div`
-    width: 100%;
-    display: flex;
-    justify-content: space-around;
-    padding: 1rem 0;
-  `;
-const TextBox = styled.div`
-display: flex;
-flex-direction: column;
-font-size: large;
-`;
-const CountNumber = styled.span`
-  text-align: center;
-`;
-const CountTitle = styled.span`
-  font-weight: 800;
-`;
+
 export default App
---
+++ src/styles.ts
@@ -0,0 +1,68 @@
+import styled from 'styled-components';
+import { motion } from '../node_modules/framer-motion';
+
+export const AppBox = styled.main`
+  width: 30rem;
+  margin-top: 30px;
+  padding: 1rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+export const TimeInput = styled.input`
+  border: none;
+  background-color: transparent;
+  width: 100%;
+  margin-left: 6px;
+  font-size: 6rem;
+`;
+export const Timer = styled.form`
+  display: flex;
+  align-items: center;
+  font-size: 6rem;
+  padding-bottom: 2rem;
+`;
+export const TimeBox = styled(motion.div)`
+  width: 8rem;
+  height: 12rem;
+  background-color: #b4c794;
+  padding: 0.5rem;
+  margin: 1rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 20px;
+  text-indent: 30%;
+`;
+export const PlayBtn = styled(motion.button)`
+  width: 4rem;
+  height: 4rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 50%;
+  font-size: 2rem;
+  border: none;
+  background-color: #dfd6cf;
+  &:hover {
+    cursor: pointer;
+  }
+`;
+export const CounterBox = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-around;
+  padding: 1rem 0;
+`;
+export const TextBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  font-size: large;
+`;
+export const CountNumber = styled.span`
+  text-align: center;
+`;
+export const CountTitle = styled.span`
+  font-weight: 800;
+`;
